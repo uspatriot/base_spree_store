d49d46a6f31761d3651ec711eeeb6a4571f5bf1e
Chris Cummings <christophermcummings@gmail.com>
2012-03-06 18:08:44 -0500

    Base Spree Store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestStore::Application.config.secret_token = 'bd2acb2d80c47a572bb1d52e0099aee59fb0649e724ec484b86208e5a39e92fbc554e41643f78f58dc5511ec340542efe16e2a9121273904a4c07895d1e1dd22'
